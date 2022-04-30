6a3247b09e41dcb5a69cf4a2fc39a31de1d1a307
Anastasia Davydova Lewis <anastasia.m.lewis@gmail.com>
2022-04-30 15:07:59 -0700

    editcracklepop

--- application/cracklepop.js
+++ application/cracklepop.js
@@ -4,18 +4,9 @@
 
 
 
-function cracklePop() {
-    for (var i = 1; i <= 100; i++) {
-        if (i % 15 === 0) {
-            console.log("CracklePop");
-        } else if (i % 3 === 0) {
-            console.log("Crackle");
-        } else if (i % 5 === 0) {
-            console.log("Pop");
-        } else {
-            console.log(i);
-        }
-    }
+for (let i = 1; i <= 100; i++) {
+  let result = '';
+  if (i % 3 === 0) result += 'Crackle';
+  if (i % 5 === 0) result += 'Pop';
+  console.log(result || i);
 }
-
-cracklePop();
