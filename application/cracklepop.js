7700569923b6265d0dfd45f854dc75164ae25262
Anastasia Davydova Lewis <anastasia.m.lewis@gmail.com>
2022-05-01 20:41:33 -0700

    small edits

--- application/cracklepop.js
+++ application/cracklepop.js
@@ -1,10 +1,9 @@
 // Write a program that prints out the numbers 1 to 100 (inclusive). 
-// If the number is divisible by 3, print _Crackle_ instead of the number. 
-// If it's divisible by 5, print _Pop_. If it's divisible by both 3 and 5, print _CracklePop_. You can use any language.
+// If the number is divisible by 3, print "Crackle" instead of the number. 
+// If it's divisible by 5, print "Pop". If it's divisible by both 3 and 5, print "CracklePop".
 
 
-
-for (let i = 1; i <= 100; i++) {
+for (i = 1; i <= 100; i++) {
   let result = '';
   if (i % 3 === 0) result += 'Crackle';
   if (i % 5 === 0) result += 'Pop';
